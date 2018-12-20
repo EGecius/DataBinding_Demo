86fc7b1007e9f91c823e2065fad5b1b4fe14032f
Egidijus Gecius <e.gecius@gmail.com>
2018-12-20 06:59:56 +0000

    add basic data binding

--- app/src/main/java/com/egecius/databindingdemo/MainActivity.kt
+++ app/src/main/java/com/egecius/databindingdemo/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.egecius.databindingdemo
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.egecius.databindingdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+
+        binding.user = User("My first name", "My last name")
     }
 }
---
+++ app/src/main/java/com/egecius/databindingdemo/User.kt
@@ -0,0 +1,4 @@
+package com.egecius.databindingdemo
+
+
+data class User(val firstName: String, val lastName: String)
