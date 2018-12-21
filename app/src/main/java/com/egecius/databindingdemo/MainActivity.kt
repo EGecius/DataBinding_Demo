772d624b9cb8172513b81ecfde4bae9f92425ef5
Egidijus Gecius <e.gecius@gmail.com>
2018-12-21 11:43:47 +0000

    make ActivityMainBinding a local variable

--- app/src/main/java/com/egecius/databindingdemo/MainActivity.kt
+++ app/src/main/java/com/egecius/databindingdemo/MainActivity.kt
@@ -8,16 +8,14 @@ import com.egecius.databindingdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    val user = User("My first name", "My last name")
-
-    lateinit var binding: ActivityMainBinding
+    private val user = User("My first name", "My last name")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
+        /* ActivityMainBinding is generated code */
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.user = user
 
         setupButton()
