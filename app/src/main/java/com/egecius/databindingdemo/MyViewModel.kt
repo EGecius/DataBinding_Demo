6c12f26563fdf9b3866cde5e80d5e137149067ec
Egidijus Gecius <e.gecius@gmail.com>
2018-12-21 14:23:58 +0000

    ad method call to ViewModel via databinding

--- app/src/main/java/com/egecius/databindingdemo/MyViewModel.kt
+++ app/src/main/java/com/egecius/databindingdemo/MyViewModel.kt
@@ -1,10 +1,20 @@
 package com.egecius.databindingdemo
 
 import androidx.databinding.ObservableField
+import androidx.databinding.ObservableInt
 import androidx.lifecycle.ViewModel
 
 class MyViewModel : ViewModel() {
 
     val firstName = ObservableField("Egis")
     val lastName = ObservableField("Gecius")
+    val counter = ObservableInt()
+
+    fun increment() {
+        counter.increment()
+    }
+
+    private fun ObservableInt.increment() {
+        set(get() + 1)
+    }
 }
