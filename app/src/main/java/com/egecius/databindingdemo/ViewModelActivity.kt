f8cae4fe1ffd37cfbffbab1116c267a0305f333c
Egidijus Gecius <e.gecius@gmail.com>
2018-12-21 14:16:37 +0000

    implement ViewModelActivity

--- app/src/main/java/com/egecius/databindingdemo/ViewModelActivity.kt
+++ app/src/main/java/com/egecius/databindingdemo/ViewModelActivity.kt
@@ -3,6 +3,7 @@ package com.egecius.databindingdemo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.egecius.databindingdemo.databinding.ActivityViewModelBinding
 
 
@@ -10,12 +11,11 @@ class ViewModelActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_model)
 
-//        val viewModel = ViewModelProviders.of(this).get(MyViewModel::class.java)
+        val viewModel = ViewModelProviders.of(this).get(MyViewModel::class.java)
 
         val binding : ActivityViewModelBinding = DataBindingUtil.setContentView(this, R.layout.activity_view_model)
-//        binding.viewModel = viewModel
+        binding.viewModel = viewModel
     }
 
 }
